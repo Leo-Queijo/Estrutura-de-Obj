510e2ee60bc2c0cabebeba126a252a78a4612435
Leo Queijo <leoqueijo8@hotmail.com>
2025-08-15 20:56:01 -0300

    Update Pilha.py

--- Pilha.py
+++ Pilha.py
@@ -6,34 +6,55 @@ class Stack:
         """ Verifica se a pilha esta vazia"""
         return len(self.items) == 0
 
-    def push(self,item):
+    def push(self, item):
         """Adiciona um item no topo da pilha"""
         self.items.append(item)
+
     def pop(self):
         """Remove e retorna o item do topo da pilha"""
         if self.is_empty():
             raise IndexError("pop from empty stack")
         return self.items.pop()
-    def peek (self):
+
+    def peek(self):
         """Retorna o item do topo da pilha sem remove-la"""
         if self.is_empty():
-            raise IndexError ("peek from empty stack")
+            raise IndexError("peek from empty stack")
         return self.items[-1]
+
     def size(self):
         """Retorna o número de itens na pilha"""
         return len(self.items)
+
     def __str__(self):
         """Retorna uma representação em string da pilha"""
-        
+
         return str(self.items)
-    
+
     def __repr__(self):
         """Retorna uma representação em string da pilha"""
-        
+
         result = ''
         for item in reversed(self.items):
             result += f'\n|{item}'
         return result
-    
-    
-        
+
+
+def verificar_parenteses(exp: str) -> bool :
+    pilha = Stack()
+
+    for c in exp:
+        if c == '(':
+            pilha.push(c)
+
+        elif c == ')':
+            if pilha.size() == 0:
+                return False
+            else:
+                pilha.pop()
+
+    return pilha.is_empty()
+
+exp = '(A + B) * (C + D)'
+print(verificar_parenteses(exp))
+
